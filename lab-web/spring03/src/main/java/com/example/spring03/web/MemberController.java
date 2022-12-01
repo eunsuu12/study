51ac2caa181fd4d6b0bf2f30c21582fcdb3a794a
eunsuu <eunsuu7@gmail.com>
2022-12-01 20:38:56 +0900

    70/121일

--- lab-web/spring03/src/main/java/com/example/spring03/web/MemberController.java
+++ lab-web/spring03/src/main/java/com/example/spring03/web/MemberController.java
@@ -18,9 +18,9 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/member") // 다른 설정이 없는 경우 GET/POST/PUT/DELETE 모든 방식의 요청을 처리.
 public class MemberController {
-    
-    private final MemberService memberService;
 
+    private final MemberService memberService;
+    
     @GetMapping("/signup") // GET 방식의 /member/signup 요청을 처리하는 메서드.
     public void signUp() {
         log.info("signUp() GET");
@@ -35,15 +35,18 @@ public class MemberController {
     public ResponseEntity<String> checkUsername(String username) {
         log.info("checkUsername(username={})", username);
         
-        String result= memberService.checkUsername(username);
+        String result = memberService.checkUsername(username);
+        
         return ResponseEntity.ok(result);
     }
     
     @PostMapping("/signup")
     public String signUp(MemberRegisterDto dto) {
-        log.info("signUp(dto= {}) POST", dto);
+        log.info("signUp(dto={}) POST", dto);
         
         memberService.registerMember(dto);
-        return "redirect:/login";
+        
+        return "redirect:/login"; // 회원 가입 성공 후 이동(redirect)
     }
+    
 }
